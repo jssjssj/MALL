ac4899cd49b05592e97b68bdff38206e30818faa
GD <GD@192.168.200.27>
2023-11-06 14:33:01 +0900

    [박겨레] Dao패키지 내부 클래스 수정

---
+++ src/main/java/dao/ManagerDao.java
@@ -0,0 +1,15 @@
+package dao;
+import vo.*;
+import java.sql.*;
+import java.util.*;
+import util.DBUtil;
+
+
+public class ManagerDao {
+	/* 디버깅 색깔 지정 */
+	// ANSI CODE   
+    final String RESET = "\u001B[0m"; 
+    final String KIM  =  "\u001B[42m";
+    final String PARK = "\u001B[44m";
+    
+}
